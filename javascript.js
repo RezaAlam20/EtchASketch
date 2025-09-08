bb3b6fdbae61b62aa360f547116971b01c61f7a2
RezaAlam20 <rzaalam384@gmail.com>
2025-09-08 18:00:23 +0330

    added colors and rainbow mode functions

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 let grid = document.querySelector(".grid")
-let n = 16
+let n = 64
 let gridLength = n ** 2 
 
 
@@ -32,6 +32,91 @@ function makeSquares(){
 
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 makeSquares()
-console.log(squareHeight)
-console.log(squareWidth)
+
+
+let squares = document.querySelectorAll(".square")
+let Drawing = false ; 
+
+function DrawingMode (){
+    squares.forEach((square)=>{
+        square.addEventListener("mousedown",()=>Drawing = true)
+        document.addEventListener("mouseup", ()=>Drawing = false)
+
+    })
+        
+}
+DrawingMode()
+
+squares.forEach ((square)=> { 
+    square.addEventListener("mouseover",()=>{
+       if(Drawing){
+        square.style.backgroundColor = color;
+       }
+       else return
+       
+    })
+ 
+})
+
+
+
+let color = "#000000";
+let chooseColor = document.querySelector("#colorSelection").addEventListener("input", function setColor(e) {
+
+    return color = e.target.value
+    
+
+})
+
+
+let squareChanged = false ;
+for(let square of squares) { 
+    square.addEventListener("mouseout" , ()=> { 
+        if (rainbowMode) { 
+            rainbows()
+        }
+    })
+}
+
+
+function rainbows(){
+    let rainbowColors = ["red" , "orange" , "violet" , "green" , "blue" , "indigo" , "violet"]
+    randomNum =Math.floor(Math.random() * 7)
+    color = rainbowColors[randomNum]
+    return 
+
+}
+
+
+
+let rainbowMode = false 
+rainbowMode = document.querySelector(".rainbow").addEventListener("click" , ()=> {
+    rainbowMode = !rainbowMode; 
+
+}) 
+
+
